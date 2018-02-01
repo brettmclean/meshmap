43f1c7ff86e2082e8de15ab48021615785d59f6e
Brett McLean <brettrmclean@gmail.com>
2018-01-31 21:46:22 -0600

    Fill out ConsoleLogProvider implementation

--- src/server/modules/logging/ConsoleLogProvider.js
+++ src/server/modules/logging/ConsoleLogProvider.js
@@ -9,4 +9,20 @@ var ConsoleLogProvider = function() {
 ConsoleLogProvider.prototype = Object.create(baseProto);
 ConsoleLogProvider.prototype.constructor = ConsoleLogProvider;
 
+ConsoleLogProvider.prototype.error = function(message) {
+	console.error(message);
+};
+ConsoleLogProvider.prototype.warn = function(message) {
+	console.warn(message);
+};
+ConsoleLogProvider.prototype.info = function(message) {
+	console.info(message);
+};
+ConsoleLogProvider.prototype.debug = function(message) {
+	console.debug(message);
+};
+ConsoleLogProvider.prototype.trace = function(message) {
+	this.debug(message);
+};
+
 module.exports = ConsoleLogProvider;
