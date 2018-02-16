ad6924f009fb5587798057796878cf6d27aa5204
Brett McLean <brettrmclean@gmail.com>
2018-02-15 18:22:06 -0600

    Create logging service as singleton

--- src/server/modules/logging/factories/loggingServiceFactory.js
+++ src/server/modules/logging/factories/loggingServiceFactory.js
@@ -2,7 +2,17 @@ var LoggingService = require("../LoggingService");
 var LogBufferService = require("../LogBufferService");
 var consoleLogProviderFactory = require("./consoleLogProviderFactory");
 
+var singletonInstance = null;
+
 var create = function() {
+	if(!singletonInstance) {
+		singletonInstance = createLoggingService();
+	}
+
+	return singletonInstance;
+};
+
+var createLoggingService = function() {
 	var deps = {
 		consoleLogProvider: consoleLogProviderFactory.create(),
 		logBufferService: new LogBufferService()
