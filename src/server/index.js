f5fd4a0e2ec34994107c393213130825e5d48f83
Brett McLean <brettrmclean@gmail.com>
2018-02-15 18:33:52 -0600

    Replace logger module with LoggingService in index.js

--- src/server/index.js
+++ src/server/index.js
@@ -1,24 +1,31 @@
 "use strict";
 
-var configLocationManagement = require("./modules/config/configLocationManagement");
+var loggingServiceFactory = require("./modules/logging/factories/loggingServiceFactory");
+var logProviderFactory = require("./modules/logging/factories/logProviderFactory");
 var appConfigServiceFactory = require("./modules/config/appConfigServiceFactory");
+var configLocationManagement = require("./modules/config/configLocationManagement");
 var fileLogLocationServiceFactory = require("./modules/logging/factories/fileLogLocationServiceFactory");
 var version = require("./modules/version");
 var busy = require("./modules/busy");
-var logger = require("./modules/logger");
 
-logger.info("Starting application" + (version ? " v" + version : "") + ".");
-logger.info("Reading config from " + configLocationManagement.getConfigDirectory());
+var loggingService = loggingServiceFactory.create();
+
+loggingService.info("Starting application" + (version ? " v" + version : "") + ".");
+loggingService.info("Reading config from " + configLocationManagement.getConfigDirectory());
 
 var appConfig = appConfigServiceFactory.create().getAppConfig();
 
-/* Start up logger */
 var fileLogLocationService = fileLogLocationServiceFactory.create(appConfig.logging.directory);
 var logDirectory = fileLogLocationService.getAbsoluteLogDirectory();
 if(logDirectory) {
-	logger.info(`Log files will be stored in ${logDirectory}`);
+	loggingService.info(`Log files will be stored in ${logDirectory}`);
 }
-logger.init(appConfig);
+
+loggingService.init(appConfig.logging);
+
+var providers = logProviderFactory.create(appConfig.logging);
+providers.forEach((provider) => provider.init());
+loggingService.setLogProviders(providers);
 
 /* Start up Site Manager */
 var sm = require("./modules/sitemanager");
@@ -33,15 +40,15 @@ var server = require("./modules/server");
 server.init();
 
 process.on("SIGINT", function() {
-	logger.info("SIGINT received.");
+	loggingService.info("SIGINT received.");
 	process.exit();
 });
 
 process.on("exit", function() {
-	logger.info("Application is shutting down.");
+	loggingService.info("Application is shutting down.");
 	busy.shutdown(); // Shutdown toobusy event loop polling
 	server.shutdown(); // Shut down HTTP/Socket.IO server
 	sm.shutdown(); // Shutdown site manager
 	store.shutdown(); // Shutdown data store
-	logger.shutdown(); // Shutdown logger
+	loggingService.shutdown();
 });
