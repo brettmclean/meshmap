5cf052721169c8d2397e05b20c79c06d1a0ec76d
Brett McLean <brettrmclean@gmail.com>
2018-02-18 23:12:42 -0600

    Create ApplicationLoadService as singleton

--- src/server/modules/appload/applicationLoadServiceFactory.js
+++ src/server/modules/appload/applicationLoadServiceFactory.js
@@ -1,7 +1,17 @@
 var ApplicationLoadService = require("./ApplicationLoadService");
 var EventLoopLagProvider = require("./EventLoopLagProvider");
 
+var singletonInstance = null;
+
 var create = function() {
+	if(!singletonInstance) {
+		singletonInstance = createApplicationLoadService();
+	}
+
+	return singletonInstance;
+};
+
+var createApplicationLoadService = function() {
 	var deps = {
 		eventLoopLagProvider: new EventLoopLagProvider()
 	};
