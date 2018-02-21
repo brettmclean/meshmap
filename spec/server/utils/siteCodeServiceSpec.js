3dc2ab32d237973174a66520f29e1ac7c59404f1
Brett McLean <brettrmclean@gmail.com>
2018-02-20 20:52:52 -0600

    Implement createSiteCode in SiteCodeService

--- spec/server/utils/siteCodeServiceSpec.js
+++ spec/server/utils/siteCodeServiceSpec.js
@@ -2,6 +2,12 @@ require("../testUtils/init");
 var loader = require("../testUtils/loader");
 
 var SiteCodeService = loader.load("utils/SiteCodeService");
+var AppConfigService = loader.load("config/AppConfigService");
+var AppConfig = loader.load("config/AppConfig");
+
+var DEFAULT_SITE_CODE_LENGTH = 7;
+
+var VALID_SITE_CODE_CHARS_REGEX = /^[0-9a-z]+$/;
 
 describe("A site code service", function() {
 
@@ -11,6 +17,63 @@ describe("A site code service", function() {
 			var scs = createSiteCodeService();
 
 			expect(typeof scs.createSiteCode).toBe("function");
+		});
+
+		it("calls getAppConfig on app config service", function() {
+			var acs = createAppConfigService(),
+				scs = createSiteCodeService({ appConfigService: acs });
+
+			scs.createSiteCode();
+
+			expect(acs.getAppConfig).toHaveBeenCalled();
+		});
+
+		it("returns a string object", function() {
+			var acs = createAppConfigService(),
+				scs = createSiteCodeService({ appConfigService: acs });
+
+			var returnedSiteCode = scs.createSiteCode();
+
+			expect(typeof returnedSiteCode).toBe("string");
+		});
+
+		it("returns 6-character site code when app config specifies site codes should be 6 characters long", function() {
+			var	siteCodeLength = 6,
+				acs = createAppConfigServiceWhichSpecifiesSiteCodeLength(siteCodeLength),
+				scs = createSiteCodeService({ appConfigService: acs });
+
+			var returnedSiteCode = scs.createSiteCode();
+
+			expect(returnedSiteCode.length).toBe(siteCodeLength);
+		});
+
+		it("returns 8-character site code when app config specifies site codes should be 8 characters long", function() {
+			var	siteCodeLength = 8,
+				acs = createAppConfigServiceWhichSpecifiesSiteCodeLength(siteCodeLength),
+				scs = createSiteCodeService({ appConfigService: acs });
+
+			var returnedSiteCode = scs.createSiteCode();
+
+			expect(returnedSiteCode.length).toBe(siteCodeLength);
+		});
+
+		it("returns a site code with only lowercase alphanumeric characters", function() {
+			var	acs = createAppConfigService(),
+				scs = createSiteCodeService({ appConfigService: acs });
+
+			var returnedSiteCode = scs.createSiteCode();
+
+			expect(VALID_SITE_CODE_CHARS_REGEX.test(returnedSiteCode)).toBe(true);
+		});
+
+		it("returns unique site codes", function() {
+			var	acs = createAppConfigService(),
+				scs = createSiteCodeService({ appConfigService: acs });
+
+			var firstSiteCode = scs.createSiteCode();
+			var secondSiteCode = scs.createSiteCode();
+
+			expect(firstSiteCode).not.toBe(secondSiteCode);
 		});
 
 	});
@@ -59,6 +122,27 @@ describe("A site code service", function() {
 
 });
 
-function createSiteCodeService() {
-	return new SiteCodeService();
+function createSiteCodeService(deps) {
+	deps = deps || {};
+	deps.appConfigService = deps.appConfigService || createAppConfigService();
+
+	return new SiteCodeService(deps);
+}
+
+function createAppConfigService() {
+	return createAppConfigServiceWhichSpecifiesSiteCodeLength(DEFAULT_SITE_CODE_LENGTH);
+}
+
+function createAppConfigServiceWhichSpecifiesSiteCodeLength(siteCodeLength) {
+	var returnedAppConfig = createAppConfigWithSiteCodeLength(siteCodeLength);
+
+	var acs = new AppConfigService({});
+	spyOn(acs, "getAppConfig").and.returnValue(returnedAppConfig);
+	return acs;
+}
+
+function createAppConfigWithSiteCodeLength(siteCodeLength) {
+	var ac = new AppConfig();
+	ac.siteCodeLength = siteCodeLength;
+	return ac;
 }
--- src/server/modules/utils/SiteCodeService.js
+++ src/server/modules/utils/SiteCodeService.js
@@ -1,11 +1,21 @@
+var SITE_CODE_CHARS = "0123456789abcdefghijklmnopqrstuvwxyz";
 var VALID_SITE_CODE_CHARS_REGEX = /^[0-9a-z]+$/;
 
-var SiteCodeService = function() {
-
+var SiteCodeService = function(deps) {
+	this._appConfigService = deps.appConfigService;
 };
 
 SiteCodeService.prototype.createSiteCode = function() {
+	var siteCode = "";
 
+	var appConfig = this._appConfigService.getAppConfig();
+
+	for(var i = 0; i < appConfig.siteCodeLength; i++) {
+		var randomIndex = Math.floor(Math.random() * SITE_CODE_CHARS.length);
+		siteCode += SITE_CODE_CHARS.charAt(randomIndex);
+	}
+
+	return siteCode;
 };
 
 SiteCodeService.prototype.isSiteCode = function(siteCode) {
