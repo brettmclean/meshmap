8976d2a5fd9108ed9362cfadf1a017f2143cd37a
Brett McLean <brettrmclean@gmail.com>
2018-02-25 21:16:37 -0600

    Make MessageHandlingService dependencies non-optional

--- spec/client/events/messageHandlingServiceSpec.js
+++ spec/client/events/messageHandlingServiceSpec.js
@@ -29,104 +29,16 @@ var STARTUP_DATA_OBJ = (function() {
 	return JSON.parse(JSON.stringify(sd));
 }());
 
-var createMessageHandlingServiceWithEventBus = function(eventBus) {
-	return new MessageHandlingService({
-		eventBus: eventBus
-	});
-};
-
-var createMessageHandlingServiceWithEventBusAndStartupDataHandler = function(eventBus, startupDataHandler) {
-	return new MessageHandlingService({
-		eventBus: eventBus,
-		startupDataHandler: startupDataHandler
-	});
-};
-
-var createMessageHandlingServiceWithEventBusAndConnectionHandler = function(eventBus, connectionHandler) {
-	return new MessageHandlingService({
-		eventBus: eventBus,
-		connectionHandler: connectionHandler
-	});
-};
-
-var createMessageHandlingServiceWithEventBusAndPageUnloadHandler = function(eventBus, pageUnloadHandler) {
-	return new MessageHandlingService({
-		eventBus: eventBus,
-		pageUnloadHandler: pageUnloadHandler
-	});
-};
-
-var createMessageHandlingServiceWithEventBusAndMapEventHandler = function(eventBus, mapEventHandler) {
-	return new MessageHandlingService({
-		eventBus: eventBus,
-		mapEventHandler: mapEventHandler
-	});
-};
-
-var createMessageHandlingServiceWithEventBusAndUserEventHandler = function(eventBus, userEventHandler) {
-	return new MessageHandlingService({
-		eventBus: eventBus,
-		userEventHandler: userEventHandler
-	});
-};
-
-var createMessageHandlingServiceWithEventBusAndChatMessageHandler = function(eventBus, chatMessageHandler) {
-	return new MessageHandlingService({
-		eventBus: eventBus,
-		chatMessageHandler: chatMessageHandler
-	});
-};
-
-var createMessageHandlingServiceWithEventBusAndErrorMessageHandler = function(eventBus, errorMessageHandler) {
-	return new MessageHandlingService({
-		eventBus: eventBus,
-		errorMessageHandler: errorMessageHandler
-	});
-};
-
-var createMessageHandlingServiceWithEventBusAndSiteSettingHandler = function(eventBus, siteSettingHandler) {
-	return new MessageHandlingService({
-		eventBus: eventBus,
-		siteSettingHandler: siteSettingHandler
-	});
-};
-
 describe("A Message Handling Service", function() {
-
-	it("does not throw an error if event bus is not provided", function() {
-		expect(function() {
-			// jshint unused: false
-			var mhs = new MessageHandlingService();
-		}).not.toThrow();
-	});
-
-	it("does not throw an error if startup data handler is not provided", function() {
-		// jshint unused: false
-		var eb = new EventBus(),
-			mhs = createMessageHandlingServiceWithEventBus(eb);
-
-		expect(function() {
-			eb.publish("startupDataReceived", STARTUP_DATA_OBJ);
-		}).not.toThrow();
-	});
-
-	it("does not throw an error if connection handler is not provided", function() {
-		// jshint unused: false
-		var eb = new EventBus(),
-			mhs = createMessageHandlingServiceWithEventBus(eb);
-
-		expect(function() {
-			eb.publish("disconnected");
-			eb.publish("connected");
-			eb.publish("pageUnloading");
-		}).not.toThrow();
-	});
 
 	it("provides a parsed StartupData object to the startup data handler", function() {
 		// jshint unused: false
-		var eb = new EventBus(),
+		var eb = createEventBus(),
 			sdh = createStartupDataHandler(),
-			mhs = createMessageHandlingServiceWithEventBusAndStartupDataHandler(eb, sdh);
+			mhs = createMessageHandlingService({
+				eventBus: eb,
+				startupDataHandler: sdh
+			});
 
 		eb.publish("startupDataReceived", STARTUP_DATA_OBJ);
 
@@ -138,11 +50,13 @@ describe("A Message Handling Service", function() {
 
 	it("defers handling a disconnection to the connection handler", function() {
 		// jshint unused: false
-		var eb = new EventBus(),
-			ch = new ConnectionHandler({}),
-			mhs = createMessageHandlingServiceWithEventBusAndConnectionHandler(eb, ch);
+		var eb = createEventBus(),
+			ch = createConnectionHandler(),
+			mhs = createMessageHandlingService({
+				eventBus: eb,
+				connectionHandler: ch
+			});
 
-		spyOn(ch, "handleDisconnect");
 		eb.publish("disconnected");
 
 		expect(ch.handleDisconnect).toHaveBeenCalled();
@@ -150,11 +64,13 @@ describe("A Message Handling Service", function() {
 
 	it("defers handling a connection to the connection handler", function() {
 		// jshint unused: false
-		var eb = new EventBus(),
-			ch = new ConnectionHandler({}),
-			mhs = createMessageHandlingServiceWithEventBusAndConnectionHandler(eb, ch);
+		var eb = createEventBus(),
+			ch = createConnectionHandler(),
+			mhs = createMessageHandlingService({
+				eventBus: eb,
+				connectionHandler: ch
+			});
 
-		spyOn(ch, "handleConnect");
 		eb.publish("connected");
 
 		expect(ch.handleConnect).toHaveBeenCalled();
@@ -162,9 +78,12 @@ describe("A Message Handling Service", function() {
 
 	it("defers page unloading to page unload handler", function() {
 		// jshint unused: false
-		var eb = new EventBus(),
+		var eb = createEventBus(),
 			puh = createPageUnloadHandler(),
-			mhs = createMessageHandlingServiceWithEventBusAndPageUnloadHandler(eb, puh);
+			mhs = createMessageHandlingService({
+				eventBus: eb,
+				pageUnloadHandler: puh
+			});
 
 		eb.publish("pageUnloading");
 
@@ -173,11 +92,13 @@ describe("A Message Handling Service", function() {
 
 	it("informs the connection handler when the page is unloading", function() {
 		// jshint unused: false
-		var eb = new EventBus(),
-			ch = new ConnectionHandler({}),
-			mhs = createMessageHandlingServiceWithEventBusAndConnectionHandler(eb, ch);
+		var eb = createEventBus(),
+			ch = createConnectionHandler(),
+			mhs = createMessageHandlingService({
+				eventBus: eb,
+				connectionHandler: ch
+			});
 
-		spyOn(ch, "setPageUnloading");
 		eb.publish("pageUnloading");
 
 		expect(ch.setPageUnloading).toHaveBeenCalled();
@@ -185,9 +106,12 @@ describe("A Message Handling Service", function() {
 
 	it("defers map events to map event handler", function() {
 		// jshint unused: false
-		var eb = new EventBus(),
+		var eb = createEventBus(),
 			meh = createMapEventHandler(),
-			mhs = createMessageHandlingServiceWithEventBusAndMapEventHandler(eb, meh),
+			mhs = createMessageHandlingService({
+				eventBus: eb,
+				mapEventHandler: meh
+			}),
 			mapEvent = new MapEvent(MapEvent.REMOVE_MARKER, 87),
 			message = new Message(Message.MAP_EVENT, mapEvent);
 
@@ -198,9 +122,12 @@ describe("A Message Handling Service", function() {
 
 	it("defers user events to user event handler", function() {
 		// jshint unused: false
-		var eb = new EventBus(),
+		var eb = createEventBus(),
 			ueh = createUserEventHandler(),
-			mhs = createMessageHandlingServiceWithEventBusAndUserEventHandler(eb, ueh),
+			mhs = createMessageHandlingService({
+				eventBus: eb,
+				userEventHandler: ueh
+			}),
 			userInfo = new UserInfo(14, "Jennifer"),
 			userEvent = new UserEvent(UserEvent.USER_CONNECT, userInfo),
 			message = new Message(Message.USER_EVENT, userEvent);
@@ -212,13 +139,15 @@ describe("A Message Handling Service", function() {
 
 	it("defers chat message events to chat message handler", function() {
 		// jshint unused: false
-		var eb = new EventBus(),
-			cmh = new ChatMessageHandler({}),
-			mhs = createMessageHandlingServiceWithEventBusAndChatMessageHandler(eb, cmh),
+		var eb = createEventBus(),
+			cmh = createChatMessageHandler(),
+			mhs = createMessageHandlingService({
+				eventBus: eb,
+				chatMessageHandler: cmh
+			}),
 			chatMessage = new ChatMessage("Hello!"),
 			message = new Message(Message.CHAT_MESSAGE, chatMessage);
 
-		spyOn(cmh, "handle");
 		eb.publish("messageReceived", message);
 
 		expect(cmh.handle).toHaveBeenCalledWith(chatMessage);
@@ -226,9 +155,12 @@ describe("A Message Handling Service", function() {
 
 	it("defers error message events to error message handler", function() {
 		// jshint unused: false
-		var eb = new EventBus(),
+		var eb = createEventBus(),
 			emh = createErrorMessageHandler(),
-			mhs = createMessageHandlingServiceWithEventBusAndErrorMessageHandler(eb, emh),
+			mhs = createMessageHandlingService({
+				eventBus: eb,
+				errorMessageHandler: emh
+			}),
 			errorMessage = "Network cable severed by honey badger",
 			message = new Message(Message.ERROR, errorMessage);
 
@@ -239,9 +171,12 @@ describe("A Message Handling Service", function() {
 
 	it("defers site setting update events to site setting handler", function() {
 		// jshint unused: false
-		var eb = new EventBus(),
+		var eb = createEventBus(),
 			ssh = createSiteSettingHandler(),
-			mhs = createMessageHandlingServiceWithEventBusAndSiteSettingHandler(eb, ssh),
+			mhs = createMessageHandlingService({
+				eventBus: eb,
+				siteSettingHandler: ssh
+			}),
 			su = new SettingUpdate(SettingUpdate.SITE_NAME, SITE_NAME),
 			message = new Message(Message.UPDATE_SITE_SETTING, su);
 
@@ -251,6 +186,34 @@ describe("A Message Handling Service", function() {
 	});
 
 });
+
+function createMessageHandlingService(deps) {
+	deps = deps || {};
+
+	deps.eventBus = deps.eventBus || new EventBus();
+	deps.connectionHandler = deps.connectionHandler || createConnectionHandler();
+	deps.startupDataHandler = deps.startupDataHandler || createStartupDataHandler();
+	deps.pageUnloadHandler = deps.pageUnloadHandler || createPageUnloadHandler();
+	deps.mapEventHandler = deps.mapEventHandler || createMapEventHandler();
+	deps.userEventHandler = deps.userEventHandler || createUserEventHandler();
+	deps.chatMessageHandler = deps.chatMessageHandler || createChatMessageHandler();
+	deps.errorMessageHandler = deps.errorMessageHandler || createErrorMessageHandler();
+	deps.siteSettingHandler = deps.siteSettingHandler || createSiteSettingHandler();
+
+	return new MessageHandlingService(deps);
+}
+
+function createEventBus() {
+	return new EventBus();
+}
+
+function createConnectionHandler() {
+	var ch = new ConnectionHandler({});
+	spyOn(ch, "handleDisconnect");
+	spyOn(ch, "handleConnect");
+	spyOn(ch, "setPageUnloading");
+	return ch;
+}
 
 function createErrorMessageHandler() {
 	var emh = new ErrorMessageHandler({});
@@ -286,4 +249,10 @@ function createUserEventHandler() {
 	var ueh = new UserEventHandler({});
 	spyOn(ueh, "handle");
 	return ueh;
+}
+
+function createChatMessageHandler() {
+	var cmh = new ChatMessageHandler({});
+	spyOn(cmh, "handle");
+	return cmh;
 }
--- src/client/events/MessageHandlingService.js
+++ src/client/events/MessageHandlingService.js
@@ -9,88 +9,64 @@ meshmap.events.MessageHandlingService = (function() {
 		UserEvent = dm.UserEvent;
 
 	var MessageHandlingService = function(deps) {
-		deps = deps || /* istanbul ignore next */ {};
-
-		this._eventBus = deps.eventBus || null;
-		this._startupDataHandler = deps.startupDataHandler || null;
-		this._connectionHandler = deps.connectionHandler || null;
-		this._pageUnloadHandler = deps.pageUnloadHandler || null;
-		this._mapEventHandler = deps.mapEventHandler || null;
-		this._userEventHandler = deps.userEventHandler || null;
-		this._chatMessageHandler = deps.chatMessageHandler || null;
-		this._errorMessageHandler = deps.errorMessageHandler || null;
-		this._siteSettingHandler = deps.siteSettingHandler || null;
+		this._eventBus = deps.eventBus;
+		this._startupDataHandler = deps.startupDataHandler;
+		this._connectionHandler = deps.connectionHandler;
+		this._pageUnloadHandler = deps.pageUnloadHandler;
+		this._mapEventHandler = deps.mapEventHandler;
+		this._userEventHandler = deps.userEventHandler;
+		this._chatMessageHandler = deps.chatMessageHandler;
+		this._errorMessageHandler = deps.errorMessageHandler;
+		this._siteSettingHandler = deps.siteSettingHandler;
 
 		subscribeToEvents.call(this);
 	};
 
 	var subscribeToEvents = function() {
-		if(this._eventBus) {
-			this._eventBus.subscribe("startupDataReceived", onStartupDataReceived.bind(this));
-			this._eventBus.subscribe("disconnected", onDisconnected.bind(this));
-			this._eventBus.subscribe("connected", onConnected.bind(this));
-			this._eventBus.subscribe("messageReceived", onMessageReceived.bind(this));
-			this._eventBus.subscribe("pageUnloading", onPageUnloading.bind(this));
-		}
+		this._eventBus.subscribe("startupDataReceived", onStartupDataReceived.bind(this));
+		this._eventBus.subscribe("disconnected", onDisconnected.bind(this));
+		this._eventBus.subscribe("connected", onConnected.bind(this));
+		this._eventBus.subscribe("messageReceived", onMessageReceived.bind(this));
+		this._eventBus.subscribe("pageUnloading", onPageUnloading.bind(this));
 	};
 
 	var onStartupDataReceived = function(startupData) {
-		if(this._startupDataHandler) {
-			startupData = StartupData.parse(startupData);
-			this._startupDataHandler.handle(startupData);
-		}
+		startupData = StartupData.parse(startupData);
+		this._startupDataHandler.handle(startupData);
 	};
 
 	var onDisconnected = function() {
-		if(this._connectionHandler) {
-			this._connectionHandler.handleDisconnect();
-		}
+		this._connectionHandler.handleDisconnect();
 	};
 
 	var onConnected = function() {
-		if(this._connectionHandler) {
-			this._connectionHandler.handleConnect();
-		}
+		this._connectionHandler.handleConnect();
 	};
 
 	var onMessageReceived = function(message) {
 		switch(message.type) {
 			case Message.MAP_EVENT:
-				if(this._mapEventHandler) {
-					this._mapEventHandler.handle(message.data);
-				}
+				this._mapEventHandler.handle(message.data);
 				break;
 			case Message.USER_EVENT:
-				if(this._userEventHandler) {
-					var userEvent = UserEvent.parse(message.data);
-					this._userEventHandler.handle(userEvent);
-				}
+				var userEvent = UserEvent.parse(message.data);
+				this._userEventHandler.handle(userEvent);
 				break;
 			case Message.CHAT_MESSAGE:
-				if(this._chatMessageHandler) {
-					this._chatMessageHandler.handle(message.data);
-				}
+				this._chatMessageHandler.handle(message.data);
 				break;
 			case Message.ERROR:
-				if(this._errorMessageHandler) {
-					this._errorMessageHandler.handle(message.data);
-				}
+				this._errorMessageHandler.handle(message.data);
 				break;
 			case Message.UPDATE_SITE_SETTING:
-				if(this._siteSettingHandler) {
-					this._siteSettingHandler.handle(message.data);
-				}
+				this._siteSettingHandler.handle(message.data);
 				break;
 		}
 	};
 
 	var onPageUnloading = function() {
-		if(this._connectionHandler) {
-			this._connectionHandler.setPageUnloading();
-		}
-		if(this._pageUnloadHandler) {
-			this._pageUnloadHandler.handle();
-		}
+		this._connectionHandler.setPageUnloading();
+		this._pageUnloadHandler.handle();
 	};
 
 	return MessageHandlingService;
